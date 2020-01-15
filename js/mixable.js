8bcbc2fc4c3a0601e4ce796bca2774b7b1821af4
Brandon Lehmann <brandonlehmann@gmail.com>
2020-01-15 18:24:05 -0500

    Filter non-pretty non-mixable amounts from the list

--- js/mixable.js
+++ js/mixable.js
@@ -5,72 +5,72 @@ $(document).ready(function () {
     paging: false,
     lengthMenu: -1,
     language: {
-      emptyTable: "Loading mixable amounts... Please wait..."
+      emptyTable: 'Loading mixable amounts... Please wait...'
     },
     columnDefs: [{
-        targets: [3],
-        type: 'num-fmt',
-        render: function (data, type, row, meta) {
-          if (type === 'display') {
-            if (data.height !== null) {
-              data = '<a href="/block.html?hash=' + data.hash + '">' + numeral(data.height).format('0,0') + '</a>'
-            } else {
-              data = ''
-            }
-          } else if (type === 'sort') {
-            if (data.height !== null) {
-              data = data.height
-            } else {
-              data = -1
-            }
+      targets: [3],
+      type: 'num-fmt',
+      render: function (data, type, row, meta) {
+        if (type === 'display') {
+          if (data.height !== null) {
+            data = '<a href="/block.html?hash=' + data.hash + '">' + numeral(data.height).format('0,0') + '</a>'
+          } else {
+            data = ''
           }
-          return data
-        }
-      },
-      {
-        targets: [1],
-        render: function (data, type, row, meta) {
-          if (type === 'display') {
-            data = numeral(data).format('0,0')
+        } else if (type === 'sort') {
+          if (data.height !== null) {
+            data = data.height
+          } else {
+            data = -1
           }
-          return data
         }
-      },
-      {
-        targets: [0],
-        render: function (data, type, row, meta) {
-          if (type === 'display') {
-            data = numeral(data / Math.pow(10, ExplorerConfig.decimalPoints)).format('0,0.00') + ' ' + ExplorerConfig.ticker
-          }
-          return data
-        }
-      },
-      {
-        targets: [2],
-        render: function (data, type, row, meta) {
-          if (type === 'display') {
-            if (data === null) {
-              data = ''
-            } else {
-              data = (new Date(data * 1000)).toGMTString()
-            }
-          }
-          return data
-        }
-      },
-      {
-        targets: [4],
-        render: function (data, type, row, meta) {
-          if (type === 'display') {
-            if (data !== null) {
-              data = '<a href="/transaction.html?hash=' + data + '">' + data + '</a>'
-            } else {
-              data = ''
-            }
-          }
-          return data
-        }
+        return data
       }
+    },
+    {
+      targets: [1],
+      render: function (data, type, row, meta) {
+        if (type === 'display') {
+          data = numeral(data).format('0,0')
+        }
+        return data
+      }
+    },
+    {
+      targets: [0],
+      render: function (data, type, row, meta) {
+        if (type === 'display') {
+          data = numeral(data / Math.pow(10, ExplorerConfig.decimalPoints)).format('0,0.00') + ' ' + ExplorerConfig.ticker
+        }
+        return data
+      }
+    },
+    {
+      targets: [2],
+      render: function (data, type, row, meta) {
+        if (type === 'display') {
+          if (data === null) {
+            data = ''
+          } else {
+            data = (new Date(data * 1000)).toGMTString()
+          }
+        }
+        return data
+      }
+    },
+    {
+      targets: [4],
+      render: function (data, type, row, meta) {
+        if (type === 'display') {
+          if (data !== null) {
+            data = '<a href="/transaction.html?hash=' + data + '">' + data + '</a>'
+          } else {
+            data = ''
+          }
+        }
+        return data
+      }
+    }
     ],
     order: [
       [0, 'asc']
@@ -81,7 +81,13 @@ $(document).ready(function () {
   getMixableAmounts()
 })
 
-function getMixableAmounts() {
+function isPrettyAmount (amount) {
+  const length = amount.toString().length
+  const base = Math.pow(10, length)
+  return (amount % base === 0)
+}
+
+function getMixableAmounts () {
   $.ajax({
     url: ExplorerConfig.apiBaseUrl + '/amounts',
     dataType: 'json',
@@ -89,8 +95,13 @@ function getMixableAmounts() {
     cache: 'true',
     success: function (data) {
       localData.mixableAmounts.clear()
+      var ignored = 0
       for (var i = 0; i < data.length; i++) {
         var denomination = data[i]
+        if (!isPrettyAmount(denomination.amount) && denomination.outputs < 4) {
+          ignored += denomination.amount
+          continue
+        }
         localData.mixableAmounts.row.add([
           denomination.amount,
           denomination.outputs,
@@ -102,6 +113,7 @@ function getMixableAmounts() {
           denomination.txnHash
         ])
       }
+      console.log('%s total shells filtered from list', ignored)
       localData.mixableAmounts.draw(false)
     },
     error: function () {
