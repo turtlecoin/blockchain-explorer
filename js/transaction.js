bfbcb2e223c71dfb603708af5c3a99c21afa33df
Brandon Lehmann <brandonlehmann@gmail.com>
2020-07-23 15:36:44 -0400

    Display extra coinbase information that was implemented in v0.28.0

--- js/transaction.js
+++ js/transaction.js
@@ -28,109 +28,140 @@ $(document).ready(function () {
     addressPrefix: ExplorerConfig.addressPrefix
   })
 
-  $.ajax({
-    url: ExplorerConfig.apiBaseUrl + '/transaction/' + hash,
-    dataType: 'json',
-    type: 'GET',
-    cache: 'false',
-    success: function (txn) {
-      $('#transactionHeaderHash').text(txn.tx.hash)
-      $('#transactionTimestamp').text((new Date(txn.block.timestamp * 1000)).toGMTString())
-      $('#transactionFee').text(numeral(txn.tx.fee / Math.pow(10, ExplorerConfig.decimalPoints)).format('0,0.00') + ' ' + ExplorerConfig.ticker)
-      $('#transactionConfirmations').text(numeral(txn.block.depth).format('0,0'))
-      $('#transactionSize').text(numeral(txn.tx.size).format('0,0') + ' bytes')
-      $('#transactionRingSize').text(numeral(txn.tx.mixin).format('0,0'))
-      $('#transactionAmount').text(numeral(txn.tx.amount_out / Math.pow(10, ExplorerConfig.decimalPoints)).format('0,0.00') + ' ' + ExplorerConfig.ticker)
-      if (txn.tx.paymentId.length !== 0) {
-        $('#transactionPaymentId').html('<a href="/paymentid.html?id=' + txn.tx.paymentId + '">' + txn.tx.paymentId + '</a>')
-      }
-      $('#blockHeight').html('<a href="/?search=' + txn.block.height + '">' + numeral(txn.block.height).format('0,0') + '</a>')
-      $('#blockHash').html('<a href="/block.html?hash=' + txn.block.hash + '">' + txn.block.hash + '</a>')
-      $('#transactionNonce').text(txn.tx.nonce)
-      $('#transactionUnlockTime').text(numeral(txn.tx.unlock_time).format('0,0'))
-      $('#transactionPublicKey').text(txn.tx.publicKey)
-      $('#inputCount').text(txn.tx.inputs.length)
-      $('#outputCount').text(txn.tx.outputs.length)
+  TurtleCoinUtils.on('ready', () => {
+    $.ajax({
+      url: ExplorerConfig.apiBaseUrl + '/transaction/' + hash,
+      dataType: 'json',
+      type: 'GET',
+      cache: 'false',
+      success: async function (txn) {
+        $('#transactionHeaderHash').text(txn.tx.hash)
+        $('#transactionTimestamp').text((new Date(txn.block.timestamp * 1000)).toGMTString())
+        $('#transactionFee').text(numeral(txn.tx.fee / Math.pow(10, ExplorerConfig.decimalPoints)).format('0,0.00') + ' ' + ExplorerConfig.ticker)
+        $('#transactionConfirmations').text(numeral(txn.block.depth).format('0,0'))
+        $('#transactionSize').text(numeral(txn.tx.size).format('0,0') + ' bytes')
+        $('#transactionRingSize').text(numeral(txn.tx.mixin).format('0,0'))
+        $('#transactionAmount').text(numeral(txn.tx.amount_out / Math.pow(10, ExplorerConfig.decimalPoints)).format('0,0.00') + ' ' + ExplorerConfig.ticker)
+        if (txn.tx.paymentId.length !== 0) {
+          $('#transactionPaymentId').html('<a href="/paymentid.html?id=' + txn.tx.paymentId + '">' + txn.tx.paymentId + '</a>')
+        }
+        $('#blockHeight').html('<a href="/?search=' + txn.block.height + '">' + numeral(txn.block.height).format('0,0') + '</a>')
+        $('#blockHash').html('<a href="/block.html?hash=' + txn.block.hash + '">' + txn.block.hash + '</a>')
+        $('#transactionNonce').text(txn.tx.nonce)
+        $('#transactionUnlockTime').text(numeral(txn.tx.unlock_time).format('0,0'))
+        $('#transactionPublicKey').text(txn.tx.publicKey)
+        $('#inputCount').text(txn.tx.inputs.length)
+        $('#outputCount').text(txn.tx.outputs.length)
 
-      const inputs = $('#inputs').DataTable({
-        columnDefs: [{
-          targets: [1, 2],
-          searchable: false
-        }, {
-          targets: [0],
-          render: function (data, type, row, meta) {
-            if (type === 'display') {
-              data = numeral(data / Math.pow(10, ExplorerConfig.decimalPoints)).format('0,0.00')
-            }
-            return data
+        /**
+         * If a transaction has 0 ring participants AND the fee is 0 AND there is only one input
+         * then it is probably a coinbase transaction and we want to display extra information
+         * that is available in the interface
+         */
+        if (txn.tx.mixin === 0 && txn.tx.fee === 0 && txn.tx.inputs.length === 1) {
+          const transaction = new TurtleCoinUtils.Transaction()
+          await transaction.parseExtra(txn.tx.extra)
+
+          if (transaction.recipientPublicSpendKey && transaction.recipientPublicViewKey) {
+            const crypto = new TurtleCoinUtils.Crypto()
+
+            TurtleCoinUtils.on('ready', async () => {
+              const fingerprint = await crypto.cn_fast_hash(
+                  transaction.recipientPublicSpendKey, transaction.recipientPublicViewKey)
+
+              $('#minerFingerprint').text(fingerprint.replace(/(..)/g, '$1:').slice(0, -1))
+
+              $('#fingerPrintRow').css('display', '')
+            })
+          }
+
+          if (transaction.poolNonceHex) {
+            $('#poolNonce').text(transaction.poolNonceHex)
+
+            $('#poolNonceRow').css('display', '')
+          }
+        }
+
+        const inputs = $('#inputs').DataTable({
+          columnDefs: [{
+            targets: [1, 2],
+            searchable: false
+          }, {
+            targets: [0],
+            render: function (data, type, row, meta) {
+              if (type === 'display') {
+                data = numeral(data / Math.pow(10, ExplorerConfig.decimalPoints)).format('0,0.00')
+              }
+              return data
+            },
+            searchable: false
+          }],
+          order: [
+            [0, 'asc'],
+            [1, 'asc']
+          ],
+          searching: false,
+          info: false,
+          paging: false,
+          lengthMenu: -1,
+          language: {
+            emptyTable: "No Transaction Inputs"
           },
-          searchable: false
-        }],
-        order: [
-          [0, 'asc'],
-          [1, 'asc']
-        ],
-        searching: false,
-        info: false,
-        paging: false,
-        lengthMenu: -1,
-        language: {
-          emptyTable: "No Transaction Inputs"
-        },
-        autoWidth: false
-      }).columns.adjust().responsive.recalc()
+          autoWidth: false
+        }).columns.adjust().responsive.recalc()
 
-      for (var i = 0; i < txn.tx.inputs.length; i++) {
-        var input = txn.tx.inputs[i]
-        inputs.row.add([
-          input.amount,
-          (input.keyImage.length === 0) ? 'Miner Reward' : input.keyImage,
-          input.type.toUpperCase()
-        ])
-      }
-      inputs.draw(false)
+        for (var i = 0; i < txn.tx.inputs.length; i++) {
+          var input = txn.tx.inputs[i]
+          inputs.row.add([
+            input.amount,
+            (input.keyImage.length === 0) ? 'Miner Reward' : input.keyImage,
+            input.type.toUpperCase()
+          ])
+        }
+        inputs.draw(false)
 
-      localData.outputs = $('#outputs').DataTable({
-        columnDefs: [{
-          targets: [0, 1, 2],
-          searchable: false
-        }, {
-          targets: [0],
-          render: function (data, type, row, meta) {
-            if (type === 'display') {
-              data = numeral(data / Math.pow(10, ExplorerConfig.decimalPoints)).format('0,0.00')
-            }
-            return data
+        localData.outputs = $('#outputs').DataTable({
+          columnDefs: [{
+            targets: [0, 1, 2],
+            searchable: false
+          }, {
+            targets: [0],
+            render: function (data, type, row, meta) {
+              if (type === 'display') {
+                data = numeral(data / Math.pow(10, ExplorerConfig.decimalPoints)).format('0,0.00')
+              }
+              return data
+            },
+            searchable: false
+          }],
+          order: [
+            [0, 'asc'],
+            [1, 'asc']
+          ],
+          searching: false,
+          info: false,
+          paging: false,
+          lengthMenu: -1,
+          language: {
+            emptyTable: "No Transaction Outputs"
           },
-          searchable: false
-        }],
-        order: [
-          [0, 'asc'],
-          [1, 'asc']
-        ],
-        searching: false,
-        info: false,
-        paging: false,
-        lengthMenu: -1,
-        language: {
-          emptyTable: "No Transaction Outputs"
-        },
-        autoWidth: false
-      }).columns.adjust().responsive.recalc()
+          autoWidth: false
+        }).columns.adjust().responsive.recalc()
 
-      for (var i = 0; i < txn.tx.outputs.length; i++) {
-        var output = txn.tx.outputs[i]
-        localData.outputs.row.add([
-          output.amount,
-          output.key,
-          output.type.toUpperCase()
-        ])
+        for (var i = 0; i < txn.tx.outputs.length; i++) {
+          var output = txn.tx.outputs[i]
+          localData.outputs.row.add([
+            output.amount,
+            output.key,
+            output.type.toUpperCase()
+          ])
+        }
+        localData.outputs.draw(false)
+      },
+      error: function () {
+        window.location = '/?search=' + hash
       }
-      localData.outputs.draw(false)
-    },
-    error: function () {
-      window.location = '/?search=' + hash
-    }
+    })
   })
 })
 
